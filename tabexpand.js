b3ccbc00fe395a999875139759632d2aecfd6ddd
Salvo <salvo.mariniello@gmail.com>
2016-05-28 18:29:34 +0200

    Update tabexpand.js

--- tabexpand.js
+++ tabexpand.js
@@ -1,131 +1,136 @@
-  /* tabExpand
-  * version 1.0 (12-05-2012)
-  * @author Salvatore Mariniello
-  * Built on top of the jQuery library
-  * http://jquery.com
-  * 
-  * https://github.com/mssalvo/tabExpand
-  * */
+/* tabExpand
+ * version 1.0 (12-05-2012)
+ * @author Salvatore Mariniello
+ * Built on top of the jQuery library
+ * http://jquery.com
+ * 
+ * https://github.com/mssalvo/tabExpand
+ * */
 
 
 
-+function($){
-  "use strict";
-  $.fn.tabExpand = function(option) {
++function($) {
+    "use strict";
+    $.fn.tabExpand = function(option) {
 
-		var defaults = {
-			colorSelect : '#FFCC00',
-			colorDeselect : '#91B5FF',
-			color : '#91B5FF',
-			cssClass:"mese",
-			cssActive:"active-ms",
-			setVisible:""
+        var defaults = {
+            colorSelect: '#FFCC00',
+            colorDeselect: '#91B5FF',
+            color: '#91B5FF',
+            cssClass: "mese",
+            cssActive: "active-ms",
+            setVisible: ""
 
-		};
+        };
 
-		var options = $.extend(defaults, option),tableID="";
+        var options = $.extend(defaults, option), tableID = "";
 
-		return this.each(function() {
-			var tr = $(this);
-			tr.bind('mouseenter mouseleave click', function(event) {
-				if (event.type == "click") {
-				if($(this).hasClass(options.cssActive)){
-				$(this).removeClass(options.cssActive);
-				var	father=$('.'+tr.attr("id"));
-				var	son=$('.'+father.attr("id"));
-				father.removeClass(options.cssActive);
-				son.removeClass(options.cssActive);
-				father.each(function(){
-					$('.'+$(this).attr("id")).each(function(){
-				  $(this).hide();
-					$('.'+$(this).attr("id")).hide();
-					});
+        return this.each(function() {
+            var tr = $(this);
+            tr.bind('mouseenter mouseleave click', function(event) {
+                if (event.type == "click") {
+                    if ($(this).hasClass(options.cssActive)) {
+                        $(this).removeClass(options.cssActive);
+                        var father = $('.' + tr.attr("id"));
+                        if (father)
+                            father.removeClass(options.cssActive);
+                        if (father.attr("id"))
+                            var son = $('.' + father.attr("id"));
+                        if (son)
+                            son.removeClass(options.cssActive);
+                        if (father)
+                            father.each(function() {
+                                if ($(this).attr("id"))
+                                    $('.' + $(this).attr("id")).each(function() {
+                                        $(this).hide();
+                                        $('.' + $(this).attr("id")).hide();
+                                    });
 
-				  });
+                            });
+                        if (father.attr("id"))
+                            $('.' + father.attr("id")).hide();
+                        $('.' + tr.attr("id")).hide();
+                        $(this).css('background', options.colorDeselect);
+                    } else {
+                        $(this).addClass(options.cssActive);
+                        $('.' + tr.attr("id")).toggle();
+                        tr.css('background', options.colorSelect);
+                    }
 
-					$('.'+father.attr("id")).hide();
-					$('.'+tr.attr("id")).hide();
-					$(this).css('backgroundColor', options.colorDeselect);
-					} else {
-					$(this).addClass(options.cssActive);
-					$('.'+tr.attr("id")).toggle();
-					tr.css('backgroundColor', options.colorSelect);
-				}
+                }
+                else if (event.type == "mouseenter") {
+                    $(this).css('background', options.colorSelect);
+                }
+                else if (event.type == "mouseleave") {
+                    if (!$(this).hasClass(options.cssActive)) {
+                        $(this).css('background', options.colorDeselect);
+                    }
+                }
 
-				}
-          else if (event.type == "mouseenter") {
-					$(this).css('backgroundColor', options.colorSelect);
-			  	}
-				  else if (event.type == "mouseleave") {
-					 if(!$(this).hasClass(options.cssActive)){
-					$(this).css('backgroundColor', options.colorDeselect);
-					 }
-				}
-
-			});
-
-
-        tr.css('backgroundColor', options.colorDeselect);
+            });
 
 
-			if(tableID=="" && options.setVisible!=""){
-	   	   tableID=tr;
-	   	    console.log(tr)
-			 $('tbody tr',tableID.parents('table')).toggle();
-       $("."+options.setVisible).toggle();
-       console.log(tableID.parents('table'))
-			}
-		});
+            tr.css('background', options.colorDeselect);
 
-		return false;
-	};
 
-	$.fn.removeColumn = function(option) {
-		var options = $.extend({th:0,td:[]}, option);
-		return this.each(function() {
-			var cld = $(this);
-			cld.bind('click', function(event) {
-				if(options.td.length>0){
+            if (tableID == "" && options.setVisible != "") {
+                tableID = tr;
+                console.log(tr)
+                $('tbody tr', tableID.parents('table')).toggle();
+                $("." + options.setVisible).toggle();
+                console.log(tableID.parents('table'))
+            }
+        });
 
-					for(var i=0;i<options.td.length;i++){
-					$('td:nth-child('+options.td[i]+')').hide();
+        return false;
+    };
 
-					}
-					if(options.th){
-						$('th:nth-child('+options.th+')').hide();
-					}
+    $.fn.removeColumn = function(option) {
+        var options = $.extend({th: 0, td: []}, option);
+        return this.each(function() {
+            var cld = $(this);
+            cld.bind('click', function(event) {
+                if (options.td.length > 0) {
 
-				}
+                    for (var i = 0; i < options.td.length; i++) {
+                        $('td:nth-child(' + options.td[i] + ')').hide();
 
-			});
+                    }
+                    if (options.th) {
+                        $('th:nth-child(' + options.th + ')').hide();
+                    }
 
-		});
+                }
 
-		return false;
+            });
 
-	};
+        });
 
-	$.fn.addRow = function(option) {
-		var options = $.extend({td:0}, option);
-				return this.each(function() {
-					var row = $(this);
-					row.bind('click', function(event) {
-					tRows="<tr class='trNew'>";
-						if(options.td){
-							for(var i=0;i<options.td;i++){
-							tRows+="<td> </td>";
-							}
-						}
-						tRows+="</tr>";
+        return false;
 
-						$('table tbody').append(tRows);
+    };
 
-					});
+    $.fn.addRow = function(option) {
+        var options = $.extend({td: 0}, option);
+        return this.each(function() {
+            var row = $(this);
+            row.bind('click', function(event) {
+                tRows = "<tr class='trNew'>";
+                if (options.td) {
+                    for (var i = 0; i < options.td; i++) {
+                        tRows += "<td> </td>";
+                    }
+                }
+                tRows += "</tr>";
 
-				});
+                $('table tbody').append(tRows);
 
-				return false;
+            });
 
-			};
+        });
+
+        return false;
+
+    };
 
 }(jQuery)
